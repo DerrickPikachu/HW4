bd36a48f78d89722f76d3b6b09d59c7c989174fb
pikaChin <derrfgh4563@gmail.com>
2019-10-28 20:41:28 +0800

    add one function complete

--- app/src/main/java/com/example/hw4/MainActivity.java
+++ app/src/main/java/com/example/hw4/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.hw4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private class Listener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.addOneBtn) {
+                currentNum++;
+                number.setText(Integer.toString(currentNum));
+            }
+        }
+    }
+
+    private int currentNum = 0;
+    private Button addOne;
+    private TextView number;
+    private Listener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listener = new Listener();
+
+        addOne = findViewById(R.id.addOneBtn);
+        number = findViewById(R.id.number);
+
+        addOne.setOnClickListener(listener);
     }
 }
